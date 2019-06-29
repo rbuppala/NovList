0e41d3b244e6c4911969f2fa769702360ef87b99
rbuppala <ramana.k.hadoop@gmail.com>
2019-06-29 15:46:34 -0400

    List even odd

---
+++ src/LinkedLists/ListEvenOdd.java
@@ -0,0 +1,57 @@
+package LinkedLists;
+/*
+Given a singly linked list, group all odd nodes together followed by the even nodes.
+Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL
+Output: 1->3->5->2->4->NULL
+Example 2:
+
+Input: 2->1->3->5->6->4->7->NULL
+Output: 2->3->6->7->1->5->4->NULL
+ */
+
+public class ListEvenOdd {
+
+    public static void main(String args[]) {
+        ListNode head = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+
+        ListNode newHead = rearrange(head);
+
+        while (newHead != null) {
+            System.out.println(newHead.val);
+            newHead = newHead.next;
+        }
+    }
+
+    public static ListNode rearrange(ListNode head) {
+        if (head != null) {
+            ListNode odd = head;
+            ListNode even = head.next;
+            ListNode evenHead = head.next;
+
+            while (even != null && even.next != null) {
+                odd.next = odd.next.next;
+                even.next = even.next.next;
+                odd = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+
+        }
+        return head;
+    }
+}
